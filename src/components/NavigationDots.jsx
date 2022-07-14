f71dece3c1a3f62f9d5e7c0955279d6b0f1f7adc
Beloise Huffstutler <me@beloisehuffstutler.codes>
2022-07-13 21:23:30 -0500

    Working Build

---
+++ src/components/NavigationDots.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavigationDots = ({ active }) => {
+  return (
+    <div className='app__navigation'>
+        {['home', 'about', 'work', 'skills', 'testimonials', 'contact'].map((item, i) => (
+        <a 
+            href={`#${item}`}
+            key={item + i}
+            className="app__navigation-dot"
+            style={active === item ? {backgroundColor: '#313BAC'} : {}}
+        />
+        ))}
+    </div>
+  )
+}
+
+export default NavigationDots
