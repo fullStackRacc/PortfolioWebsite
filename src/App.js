772fad0b38edd768277e407021b14d7f7077e74c
Beloise Huffstutler <me@beloisehuffstutler.codes>
2022-07-14 08:57:37 -0500

    quick fixes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
 
 import { About, Footer, Header, Skills, Testimonial, Work } from './container';
 import { Navbar } from './components';
 import './App.scss'
+import { lightTheme, darkTheme, GlobalStyles } from "./themes.js"
+
+const StyledApp = styled.div;
 
 const App = () => {
+  const [theme, setTheme] = useState('light')
+
+  const themeToggler = _ => {
+    theme === "light" ? setTheme('dark') : setTheme('light')
+  }
+
   return (
-    <div>
-      <Navbar />
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <StyledApp>
+        <h1>Hello World</h1>
+        <button onClick={() =>themeToggler()}>Change Theme</button>
+      </StyledApp>
+
+      {/* <Navbar />
       <Header />
       <About />
       <Work />
       <Skills />
       <Testimonial />
-      <Footer />
-    </div>
+      <Footer /> */}
+    </ThemeProvider>
+
   )
 }
 
---
+++ src/themes.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const lightTheme = {
+    body: '#fff',
+    fontColor: '#000'
+}
+
+export const darkTheme = {
+    body: '#000',
+    fontColor: "fff"
+}
+
+export const GlobalStyles = createGlobalStyle`
+    body {
+        background: ${(props) => props.theme.body}
+        color: ${(props) => props.theme.fontColor}
+    }
+
+`
