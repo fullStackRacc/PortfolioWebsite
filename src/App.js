7435d77e22d514e22efb68ee72c6f4afa339f133
Beloise Huffstutler <me@beloisehuffstutler.codes>
2022-07-14 09:51:07 -0500

    MVP portfolio

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
-import React, { useState } from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import React from 'react';
 
 import { About, Footer, Header, Skills, Testimonial, Work } from './container';
 import { Navbar } from './components';
 import './App.scss'
-import { lightTheme, darkTheme, GlobalStyles } from "./themes.js"
-
-const StyledApp = styled.div;
 
 const App = () => {
-  const [theme, setTheme] = useState('light')
-
-  const themeToggler = _ => {
-    theme === "light" ? setTheme('dark') : setTheme('light')
-  }
-
   return (
-    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-      <GlobalStyles />
-      <StyledApp>
-        <h1>Hello World</h1>
-        <button onClick={() =>themeToggler()}>Change Theme</button>
-      </StyledApp>
-
-      {/* <Navbar />
+    <div>
+      <Navbar />
       <Header />
       <About />
       <Work />
       <Skills />
       <Testimonial />
-      <Footer /> */}
-    </ThemeProvider>
-
+      <Footer />
+    </div>
   )
 }
 
